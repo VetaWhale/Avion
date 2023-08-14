5bb634c7b79e656af416f5b49923d0222fe0f689
Elizaveta Shkimbova <veta-whale@yandex.ru>
2023-08-14 14:05:20 +0300

    added adaptive for menu

---
+++ index.js
@@ -0,0 +1,44 @@
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME = 'body_fixed';
+
+const bodyNode = document.querySelector('body');
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger__content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
+const POPUP_OPENED_CLASSNAME = 'popup_open';
+const popupNode = document.querySelector('.js-popup');
+const popupContentNode = document.querySelector('.js-popup__content');
+const btnOpenNode = document.querySelector('.js-btn');
+const btnCloseNode = document.querySelector('.js-popup__close-btn');
+
+btnOpenNode.addEventListener('click', togglePopup)
+btnCloseNode.addEventListener('click', togglePopup)
+
+popupNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
+
+    if (isClickOutsideContent) {
+        togglePopup();
+    }
+})
+
+function togglePopup() {
+    popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+}
